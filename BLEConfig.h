6c125f7aed5d4fa254a70a524a5940ef42400345
Carsten Koester <carsten@ckoester.net>
2025-04-22 02:30:12 -0700

    Refactor to use ESP32 BLE library
    Use ESP32 BLE library instead of ArduinoBLE. Not sure yet if this was a
    smart move or not...

--- BLEConfig.h
+++ BLEConfig.h
@@ -1,32 +1,41 @@
 #ifndef BLEConfig_h
 #define BLEConfig_h
 
-#include "Arduino.h"
 #include <Preferences.h>
-#include <ArduinoBLE.h>
-#include <map>
+#include <string>
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
 
 #include "BLEConfigItem.h"
 
 class BLEConfig
 {
+  class ServerCallbacks : public BLEServerCallbacks {
+    public:
+      ServerCallbacks(BLEConfig *pBLEConfig);
+      void onConnect(BLEServer *pServer);
+      void onDisconnect(BLEServer *pServer);
+    protected:
+      BLEConfig* _pBLEConfig;
+  };
+
   public:
     BLEConfig();
 
-    void setAppName(const char* appName, bool uniqueName=true);
     void addItem(BLEConfigItem& item);
 
-    bool begin();
-    bool begin(BLEConfigItemList items);
+    bool begin(const char* appName, bool uniqueName=true);
+    bool begin(const char* appName, bool uniqueName, BLEConfigItemList items);
     void loop();
-    void poll();
-    bool connected() const { return (_active && BLE.connected()); };
-    void handleConnected();
-    static void pollAndHandleConnected();
     void advertise();
     void end();
 
+    inline const bool connected() const { return(_connected); };
     inline const bool active() const { return(_active); };
+    inline const unsigned long getConnectionCount() const { return(_connectionCount); };
+
 
     static Preferences preferences;
 
@@ -34,11 +43,17 @@ class BLEConfig
     static constexpr const char* UUID_TEMPLATE = "84fdef37-%04x-41e6-adbd-f87ced6f2e5b";
 
   private:
-    BLEService _service;
+    BLEServer* _pServer;
+    BLEService* _pService;
+    BLEAdvertising* _pAdvertising;
+
     const char* _appName;
     bool _uniqueName;
     char* _appNameWithMac;
-    bool _active;
+
+    bool _active = false;
+    bool _connected = false;
+    unsigned long _connectionCount = 0;
 };
 
 #endif
--- BLEConfigItem.h
+++ BLEConfigItem.h
@@ -1,37 +1,58 @@
 #ifndef BLEConfigItem_h
 #define BLEConfigItem_h
 
-#include <ArduinoBLE.h>
 #include <map>
+#include <string>
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2901.h>  // User Description descriptor
+#include <BLE2904.h>  // Presentation Format descriptor
 
 class BLEConfigItem
 {
   public:
     BLEConfigItem(const char* name, unsigned int size);
-    inline BLECharacteristic* getCharacteristic() const { return _characteristic; };
+    void addToService(BLEService* pService);
+
+    inline const BLECharacteristic* getCharacteristic() const { return _pCharacteristic; };
     inline const char* getName() const { return _name; };
-    static void characteristicWritten(BLEDevice central, BLECharacteristic characteristic);
 
-    static unsigned int itemNumber;
-
+    static unsigned int itemCount;
 
   protected:
-    BLECharacteristic* _characteristic;
+    BLECharacteristic* _pCharacteristic;
+    BLE2901* _pBLE2901;
+
     const char* _name;
+    unsigned int _size;
 
-    static std::map<String, BLEConfigItem*> _byName;
-    static std::map<String, BLEConfigItem*> _byUUID;
+    static std::map<std::string, BLEConfigItem*> _byName;
+    static std::map<std::string, BLEConfigItem*> _byUUID;
 
-    virtual void writeHandler(const char* value, unsigned int size) = 0;
-    virtual void loadPreferences() = 0;
+    virtual void writeHandler(String value) = 0;
+    virtual void load() = 0;
 
     char _uuid[37];
+
+    class CharacteristicCallbacks : public BLECharacteristicCallbacks {
+      public:
+        CharacteristicCallbacks(BLEConfigItem* pConfigItem);
+        void onWrite(BLECharacteristic *pCharacteristic);
+
+      protected:
+        BLEConfigItem* _pConfigItem;
+    };
 
   friend class BLEConfig;
 };
 
 using BLEConfigItemList = std::initializer_list<BLEConfigItem>;
 
+/*
+ * Unsigned Integer
+ */
 class BLEUIntConfigItem : public BLEConfigItem
 {
   public:
@@ -43,7 +64,30 @@ class BLEUIntConfigItem : public BLEConfigItem
     unsigned int _defaultValue;
 
   protected:
-    void writeHandler(const char* value, unsigned int size);
-    void loadPreferences();
+    void writeHandler(String value);
+    void load();
+
+    BLE2904* _pBLE2904;
+};
+
+/*
+ * String
+ */
+class BLEStringConfigItem : public BLEConfigItem
+{
+  public:
+    BLEStringConfigItem(const char* name, std::string defaultValue, unsigned int max_length);
+    inline const std::string value() const { return(_value); };
+
+  private:
+    std::string _value;
+    std::string _defaultValue;
+    unsigned int _max_length;
+
+  protected:
+    void writeHandler(String value);
+    void load();
+
+    BLE2904* _pBLE2904;
 };
 #endif
